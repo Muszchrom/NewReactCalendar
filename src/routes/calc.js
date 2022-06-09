417f15cd024d126a88827be41f9754d8fba209dd
Muszchrom <altair2468@o2.pl>
2022-06-09 15:52:56 +0200

    fixin day 0 bugs

--- src/routes/calc.js
+++ src/routes/calc.js
@@ -152,17 +152,28 @@ export default function Calc(props) {
     } else {
       setWynik(`(${rez} ${operator} j${imz})`);
     }
+    setStartObliczania(2);
+  }
+
+  const resetuj = () => {
+    setInputRez(0);
+    setInputImz(0);
+    setData([]);
+    setStartObliczania(0);
+    setWynik("");
   }
 
   return (
-    <div style={{margin: "1em"}}>
+    <div style={{margin: "1em", maxWidth: "calc(500px + 2em)"}}>
       <h1>Kalkulator Podstawowy</h1>
       <div style={{display: "flex", flexDirection: "column"}}>
         <div className="calc-container-1">
         <input className="text-input" value={inputRez} type="text" onChange={e => setInputRez(e.target.value)}/>
           <span style={{fontSize: "1.6em", marginLeft: "0px"}}>+</span>
-          <input className="text-input" value={inputImz} type="text" onChange={e => setInputImz(e.target.value)}/>
-          <span style={{fontSize: "1.6em", marginLeft: "-20px"}}>i</span>
+          <div style={{width: "40%", display: "flex"}}>
+            <span style={{fontSize: "1.6em"}}>j</span>
+            <input style={{width: "94%", marginLeft: "auto"}} className="text-input" value={inputImz} type="text" onChange={e => setInputImz(e.target.value)}/>
+          </div>
         </div>
         <div className="calc-container-3">
           {data.map((obj, index) => {
@@ -188,8 +199,10 @@ export default function Calc(props) {
         </div>
         {
           startObliczania === 0 ?
-          (<button style={{marginTop: "1em"}} className="fancy-button" onClick={runPotwierdz}>Potwierdz</button>) :
-          (<button style={{marginTop: "1em"}} className="fancy-button" onClick={obliczanie}>Oblicz</button>)
+          (<button style={{marginTop: "1em"}} className="fancy-button" onClick={runPotwierdz}>Potwierdz</button>) : (
+            startObliczania === 1 ? (<button style={{marginTop: "1em"}} className="fancy-button" onClick={obliczanie}>Oblicz</button>) :
+            (<button style={{marginTop: "1em"}} className="fancy-button" onClick={resetuj}>Resetuj</button>)
+          )
 
         }
       </div>
@@ -206,13 +219,15 @@ function Wyznacznik(props) {
   const [wynik, setWynik] = useState("");
 
   const oblicz = () => {
-    setA(parseFloat(a[0]), parseFloat(a[1]));
-    setB(parseFloat(b[0]), parseFloat(b[1]));
-    setC(parseFloat(c[0]), parseFloat(c[1]));
-    setD(parseFloat(d[0]), parseFloat(d[1]));
+    setA([parseFloat(a[0]), parseFloat(a[1])]);
+    setB([parseFloat(b[0]), parseFloat(b[1])]);
+    setC([parseFloat(c[0]), parseFloat(c[1])]);
+    setD([parseFloat(d[0]), parseFloat(d[1])]);
     let [xRez, xImz] = props.mnozenie(a[0], a[1], d[0], d[1]);
     let [yRez, yImz] = props.mnozenie(b[0], b[1], c[0], c[1]);
     let [rez, imz] = props.odejmowanie(xRez, xImz, yRez, yImz);
+
+    console.log(a);
 
     let operator;
     if (imz >=0) {
@@ -241,31 +256,31 @@ function Wyznacznik(props) {
       <h1>Wyznacznik macierzy 2x2</h1>
       <div style={{display: "flex", flexDirection: "column"}}>
         <div style={{marginBottom: 0, borderLeft: "2px solid var(--text3)", borderRight: "2px solid var(--text3)"}} className="calc-container-1">
-          <div style={{position: "relative"}} className="calc-container-1">
+          <div className="calc-container-1">
             <input className="text-input" type="text" value={a[0]} onChange={e => setA([e.target.value, a[1]])}/>
             <span style={{fontSize: "1.6em", marginRight: "0px"}}>+</span>
+            <span style={{fontSize: "1.2em"}}>j</span>
             <input className="text-input" type="text" value={a[1]} onChange={e => setA([a[0], e.target.value])}/>
-            <span style={{fontSize: "1.2em", position: "absolute", right: "40px"}}>j</span>
           </div>
-          <div style={{position: "relative"}} className="calc-container-1">
+          <div className="calc-container-1">
             <input className="text-input" type="text" value={b[0]} onChange={e => setB([e.target.value, b[1]])}/>
             <span style={{fontSize: "1.6em", marginRight: "0px"}}>+</span>
+            <span style={{fontSize: "1.2em"}}>j</span>
             <input className="text-input" type="text" value={b[1]} onChange={e => setB([b[0], e.target.value])}/>
-            <span style={{fontSize: "1.2em", position: "absolute", right: "40px"}}>j</span>
           </div>
         </div>
         <div style={{marginTop: 0, borderLeft: "2px solid var(--text3)", borderRight: "2px solid var(--text3)"}} className="calc-container-1">
-          <div style={{position: "relative"}} className="calc-container-1">
+          <div className="calc-container-1">
             <input className="text-input" type="text" value={c[0]} onChange={e => setC([e.target.value, c[1]])}/>
             <span style={{fontSize: "1.6em", marginRight: "0px"}}>+</span>
+            <span style={{fontSize: "1.2em"}}>j</span>
             <input className="text-input" type="text" value={c[1]} onChange={e => setC([c[0], e.target.value])}/>
-            <span style={{fontSize: "1.2em", position: "absolute", right: "40px"}}>j</span>
           </div>
-          <div style={{position: "relative"}} className="calc-container-1">
+          <div className="calc-container-1">
             <input className="text-input" type="text" value={d[0]} onChange={e => setD([e.target.value, d[1]])}/>
             <span style={{fontSize: "1.6em", marginRight: "0px"}}>+</span>
-            <input className="text-input" type="text" value={d[1]} onChange={e => setD([d[1], e.target.value])}/>
-            <span style={{fontSize: "1.2em", position: "absolute", right: "40px"}}>j</span>
+            <span style={{fontSize: "1.2em"}}>j</span>
+            <input className="text-input" type="text" value={d[1]} onChange={e => setD([d[0], e.target.value])}/>
           </div>
         </div>
         {wynik && (<span style={{margin: "10px auto"}}>wynik: {wynik}</span>)}
